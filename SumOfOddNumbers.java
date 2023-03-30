3ae90b4406576c24c4a42f38d18e48a1c305c326
ManceRayder254 <rakeychris69@gmail.com>
2023-03-31 02:24:43 +0300

    Updating function

--- SumOfOddNumbers.java
+++ SumOfOddNumbers.java
@@ -1,9 +1,9 @@
 public class SumOfOddNumbers {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0; i <= 100; i += 2) {
+        for (int i = 0; i <= 1000; i += 2) {
             sum += i;
         }
-        System.out.println("The sum of the first 100 odd numbers is " + sum);
+        System.out.println("The sum of the first 1000 odd numbers is " + sum);
     }
 }
